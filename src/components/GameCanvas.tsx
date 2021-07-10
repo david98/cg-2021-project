2cf7a0fc171374c8091147580861414502ed4bfe
david98 <davidevolta98@gmail.com>
2021-07-10 13:20:35 +0200

    add skybox

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { sampleFragment, sampleVertex, load as loadShaders } from '../shaders'
+import {
+    sampleFragment,
+    sampleVertex,
+    skyboxVertex,
+    skyboxFragment,
+    load as loadShaders,
+} from '../shaders'
 import { flowerModel, tree1Model, load as loadModels } from '../models'
 import { createProgram, createShader, degToRad } from '../utils'
 import { OBJ } from 'webgl-obj-loader'
@@ -41,6 +47,22 @@ let turnAmounts = {
     x: 0,
     y: 0,
 }
+
+// Shaders
+let vegetationVertexShader: WebGLShader | null | undefined = null
+let vegetationFragmentShader: WebGLShader | null | undefined = null
+let skyboxVertexShader: WebGLShader | null | undefined = null
+let skyboxFragmentShader: WebGLShader | null | undefined = null
+
+let vegetationProgram: WebGLProgram | null | undefined = null
+let skyboxProgram: WebGLProgram | null | undefined = null
+
+// Shaders Locations
+let skyboxVAO: WebGLVertexArrayObject | null = null
+let skyboxPositionLocation: number | null = null
+let skyboxLocation: WebGLUniformLocation | null = null
+let skyboxViewDirectionProjectionInverseLocation: WebGLUniformLocation | null =
+    null
 
 export function GameCanvas() {
     const cRef = useRef<HTMLCanvasElement>(null)
@@ -86,14 +108,89 @@ export function GameCanvas() {
         }
     }
 
+    const loadSkybox = (gl: WebGL2RenderingContext) => {
+        // Create a texture.
+        var texture = gl.createTexture()
+        gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture)
+
+        const faceInfos = [
+            {
+                target: gl.TEXTURE_CUBE_MAP_POSITIVE_X,
+                url: 'https://webgl2fundamentals.org/webgl/resources/images/computer-history-museum/pos-x.jpg',
+            },
+            {
+                target: gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
+                url: 'https://webgl2fundamentals.org/webgl/resources/images/computer-history-museum/neg-x.jpg',
+            },
+            {
+                target: gl.TEXTURE_CUBE_MAP_POSITIVE_Y,
+                url: 'https://webgl2fundamentals.org/webgl/resources/images/computer-history-museum/pos-y.jpg',
+            },
+            {
+                target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Y,
+                url: 'https://webgl2fundamentals.org/webgl/resources/images/computer-history-museum/neg-y.jpg',
+            },
+            {
+                target: gl.TEXTURE_CUBE_MAP_POSITIVE_Z,
+                url: 'https://webgl2fundamentals.org/webgl/resources/images/computer-history-museum/pos-z.jpg',
+            },
+            {
+                target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Z,
+                url: 'https://webgl2fundamentals.org/webgl/resources/images/computer-history-museum/neg-z.jpg',
+            },
+        ]
+        faceInfos.forEach((faceInfo) => {
+            const { target, url } = faceInfo
+
+            // Upload the canvas to the cubemap face.
+            const level = 0
+            const internalFormat = gl.RGBA
+            const width = 512
+            const height = 512
+            const format = gl.RGBA
+            const type = gl.UNSIGNED_BYTE
+
+            // setup each face so it's immediately renderable
+            gl.texImage2D(
+                target,
+                level,
+                internalFormat,
+                width,
+                height,
+                0,
+                format,
+                type,
+                null
+            )
+
+            // Asynchronously load an image
+            const image = new Image()
+            image.src = url
+            image.crossOrigin = ''
+            image.addEventListener('load', function () {
+                // Now that the image has loaded make copy it to the texture.
+                gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture)
+                gl.texImage2D(
+                    target,
+                    level,
+                    internalFormat,
+                    format,
+                    type,
+                    image
+                )
+                gl.generateMipmap(gl.TEXTURE_CUBE_MAP)
+            })
+        })
+        gl.generateMipmap(gl.TEXTURE_CUBE_MAP)
+        gl.texParameteri(
+            gl.TEXTURE_CUBE_MAP,
+            gl.TEXTURE_MIN_FILTER,
+            gl.LINEAR_MIPMAP_LINEAR
+        )
+    }
+
     const init = () => {
         if (gl) {
-            let vertexShader = createShader(gl, gl.VERTEX_SHADER, sampleVertex)
-            let fragmentShader = createShader(
-                gl,
-                gl.FRAGMENT_SHADER,
-                sampleFragment
-            )
             flowerObj.mesh = flowerModel
             treeObj.mesh = tree1Model
             initMeshBuffers(gl, root)
@@ -111,7 +208,73 @@ export function GameCanvas() {
                 turnAmounts.y = -e.movementX
             })
 
-            program = createProgram(gl, vertexShader, fragmentShader)
+            skyboxVertexShader = createShader(
+                gl,
+                gl.VERTEX_SHADER,
+                skyboxVertex
+            )
+            skyboxFragmentShader = createShader(
+                gl,
+                gl.FRAGMENT_SHADER,
+                skyboxFragment
+            )
+
+            if (!skyboxVertexShader || !skyboxFragmentShader) {
+                console.error('Missing skybox shaders')
+                return
+            }
+
+            skyboxProgram = createProgram(
+                gl,
+                skyboxVertexShader,
+                skyboxFragmentShader
+            )
+
+            if (!skyboxProgram) {
+                console.error('Error while creating skybox program')
+                return
+            }
+
+            vegetationVertexShader = createShader(
+                gl,
+                gl.VERTEX_SHADER,
+                sampleVertex
+            )
+            vegetationFragmentShader = createShader(
+                gl,
+                gl.FRAGMENT_SHADER,
+                sampleFragment
+            )
+
+            if (!vegetationVertexShader || !vegetationFragmentShader) {
+                console.error('Missing vegetation shaders')
+                return
+            }
+
+            vegetationProgram = createProgram(
+                gl,
+                vegetationVertexShader,
+                vegetationFragmentShader
+            )
+
+            if (!vegetationProgram) {
+                console.error('Error while creating vegetation program')
+                return
+            }
+
+            skyboxVAO = gl.createVertexArray()
+            skyboxPositionLocation = gl.getAttribLocation(
+                skyboxProgram,
+                'a_position'
+            )
+            skyboxLocation = gl.getUniformLocation(skyboxProgram, 'u_skybox')
+            skyboxViewDirectionProjectionInverseLocation =
+                gl.getUniformLocation(
+                    skyboxProgram,
+                    'u_viewDirectionProjectionInverse'
+                )
+
+            loadSkybox(gl)
         }
     }
 
@@ -242,19 +405,24 @@ export function GameCanvas() {
         const zFar = 100
         const fovRadians = degToRad(60)
 
-        if (gl && program) {
-            // Tell it to use our program (pair of shaders)
-            gl.useProgram(program)
-
+        if (gl) {
             gl.enable(gl.DEPTH_TEST)
-
             gl.enable(gl.CULL_FACE)
+
+            // Clear the canvas
+            gl.clearColor(0, 0, 0, 1)
+            gl.clear(gl.DEPTH_BUFFER_BIT)
+            gl.clear(gl.COLOR_BUFFER_BIT)
 
             handleInput(deltaTime)
             let aspect = gl.canvas.width / gl.canvas.height
             if ('clientWidth' in gl.canvas) {
                 aspect = gl.canvas.clientWidth / gl.canvas.clientHeight
             }
+
+            gl.canvas.height = window.innerHeight
+            gl.canvas.width = window.innerWidth
+            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
             const projMat = new Matrix4().perspective({
                 fov: fovRadians,
@@ -268,11 +436,94 @@ export function GameCanvas() {
             // Make a view matrix from the camera matrix.
             const view = camera.invert()
 
-            let worldMatLoc = gl.getUniformLocation(program, 'u_world')
-            let viewMatLoc = gl.getUniformLocation(program, 'u_view')
-            let projMatLoc = gl.getUniformLocation(program, 'u_proj')
+            // Draw skybox
+            if (!skyboxProgram) {
+                console.error('Skybox program error')
+                return
+            }
+
+            gl.useProgram(skyboxProgram)
+
+            // and make it the one we're currently working with
+            gl.bindVertexArray(skyboxVAO)
+
+            // Create a buffer for positions
+            let positionBuffer = gl.createBuffer()
+            // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+            // Put the positions in the buffer
+            let positions = new Float32Array([
+                -1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1,
+            ])
+            gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW)
+
+            if (skyboxPositionLocation === null) {
+                console.error('Skybox position location is null')
+                return
+            }
+
+            // Turn on the position attribute
+            gl.enableVertexAttribArray(skyboxPositionLocation)
+
+            // Bind the position buffer.
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+            // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+            let size = 2 // 2 components per iteration
+            let type = gl.FLOAT // the data is 32bit floats
+            let normalize = false // don't normalize the data
+            let stride = 0 // 0 = move forward size * sizeof(type) each iteration to get the next position
+            let offset = 0 // start at the beginning of the buffer
+            gl.vertexAttribPointer(
+                skyboxPositionLocation,
+                size,
+                type,
+                normalize,
+                stride,
+                offset
+            )
+
+            let viewDirectionProjectionMatrix = projMat
+                .clone()
+                .multiplyRight(
+                    new Matrix4().fromQuaternion(
+                        cameraObj.orientation.clone().invert()
+                    )
+                )
+            let viewDirectionProjectionInverseMatrix =
+                viewDirectionProjectionMatrix.invert()
+
+            // Set the uniforms
+            gl.uniformMatrix4fv(
+                skyboxViewDirectionProjectionInverseLocation,
+                false,
+                viewDirectionProjectionInverseMatrix
+            )
+
+            // Tell the shader to use texture unit 0 for u_skybox
+            gl.uniform1i(skyboxLocation, 0)
+
+            // let our quad pass the depth test at 1.0
+            gl.depthFunc(gl.LEQUAL)
+
+            // Draw the geometry.
+            gl.drawArrays(gl.TRIANGLES, 0, 6)
+
+            // Draw vegetation
+            if (!vegetationProgram) {
+                console.error('Vegetation program error')
+                return
+            }
+            gl.useProgram(vegetationProgram)
+
+            let worldMatLoc = gl.getUniformLocation(
+                vegetationProgram,
+                'u_world'
+            )
+            let viewMatLoc = gl.getUniformLocation(vegetationProgram, 'u_view')
+            let projMatLoc = gl.getUniformLocation(vegetationProgram, 'u_proj')
             let dirLightDirLoc = gl.getUniformLocation(
-                program,
+                vegetationProgram,
                 'u_directionalLightDir'
             )
 
@@ -281,32 +532,23 @@ export function GameCanvas() {
             gl.uniform3fv(dirLightDirLoc, directionalLightDir)
 
             let positionAttributeLocation = gl.getAttribLocation(
-                program,
+                vegetationProgram,
                 'a_position'
             )
             let normalAttributeLocation = gl.getAttribLocation(
-                program,
+                vegetationProgram,
                 'a_normal'
             )
 
             gl.enableVertexAttribArray(positionAttributeLocation)
             gl.enableVertexAttribArray(normalAttributeLocation)
 
-            gl.canvas.height = window.innerHeight
-            gl.canvas.width = window.innerWidth
-            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-
-            // Clear the canvas
-            gl.clearColor(0, 0, 0, 1)
-            gl.clear(gl.DEPTH_BUFFER_BIT)
-            gl.clear(gl.COLOR_BUFFER_BIT)
-
             treeObj.orientation.rotateY(degToRad(2) * deltaTime)
             flowerObj.orientation.rotateY(degToRad(10) * deltaTime)
 
             renderRecursive(
                 gl,
-                program,
+                vegetationProgram,
                 root,
                 positionAttributeLocation,
                 normalAttributeLocation,
--- src/shaders/index.ts
+++ src/shaders/index.ts
@@ -2,9 +2,15 @@
 import sample_fragment from './sample_fragment.glsl'
 // @ts-ignore
 import sample_vertex from './sample_vertex.glsl'
+// @ts-ignore
+import skybox_vertex from './skybox_vertex.glsl'
+// @ts-ignore
+import skybox_fragment from './skybox_fragment.glsl'
 
 export let sampleFragment = sample_fragment
 export let sampleVertex = sample_vertex
+export let skyboxVertex = skybox_vertex
+export let skyboxFragment = skybox_fragment
 
 export let loaded = false
 
@@ -16,6 +22,8 @@ export const load = async () => {
     if (!loaded) {
         sampleFragment = await loadShader(sampleFragment)
         sampleVertex = await loadShader(sampleVertex)
+        skyboxVertex = await loadShader(skyboxVertex)
+        skyboxFragment = await loadShader(skyboxFragment)
 
         loaded = true
     }
